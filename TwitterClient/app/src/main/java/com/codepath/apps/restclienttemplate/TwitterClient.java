803673227562b7b648b43e784eab654fbe224942
Greeshma Avadhootha <ava.greeshma13@gmail.com>
2020-10-10 02:32:42 -0400

    Demo image

--- TwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ TwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -30,6 +30,10 @@ public class TwitterClient extends OAuthBaseClient {
 	public static final String REST_CONSUMER_KEY = "IXfveGUuyw36f3cb0IglzJKag";       // Change this inside apikey.properties
 	public static final String REST_CONSUMER_SECRET = "YHwAh2BBpTfntVEnTmXLks0tpW0WcdajhfCAhIoGyMWKCEdBjd"; // Change this inside apikey.properties
 
+//	public static final String REST_CONSUMER_KEY = "4KxocRp2Wh8RZ9cy1KJEjxGVy ";       // Change this inside apikey.properties
+//	public static final String REST_CONSUMER_SECRET = "EeyJ4vEZN3al7c0C13bMwAY3pGc2RASrampYtvJvnX1kLDHKJf"; // Change this inside apikey.properties
+
+
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
 
